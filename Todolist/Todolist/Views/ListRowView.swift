d5c3fc8dd2728dbf4be6fea3fbd204dba0287a22
WDI_JSON <wouterdijks@hotmail.com>
2022-02-27 16:49:58 +0100

    Aanmaken views voor basis MVVM model.

---
+++ Todolist/Todolist/Views/ListRowView.swift
@@ -0,0 +1,26 @@
+//
+//  ListRowView.swift
+//  Todolist
+//
+//  Created by Wouter on 27/02/2022.
+//
+
+import SwiftUI
+
+struct ListRowView: View {
+    let title: String
+    
+    var body: some View {
+        HStack{
+            Image(systemName: "checkmark.circle")
+            Text(title)
+            Spacer()
+        }
+    }
+}
+
+struct ListRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        ListRowView(title: "This is the first title!")
+    }
+}
