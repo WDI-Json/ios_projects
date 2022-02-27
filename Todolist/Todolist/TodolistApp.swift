8e2bf1050219fab964198be30ab52487837e82be
WDI_JSON <wouterdijks@hotmail.com>
2022-02-27 15:57:15 +0100

    aanmaak project todolist

---
+++ Todolist/Todolist/TodolistApp.swift
@@ -0,0 +1,24 @@
+//
+//  TodolistApp.swift
+//  Todolist
+//
+//  Created by Wouter on 27/02/2022.
+//
+
+import SwiftUI
+/*
+ MVVM architectuur
+ 
+ Model - data point (database objects)
+ View - UI
+ ViewModel - class that manages the model for the view.
+ 
+ */
+@main
+struct TodolistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
