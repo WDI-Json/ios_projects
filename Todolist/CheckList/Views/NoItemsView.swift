882db545976cf62f117059ca6630e60ae7ad36c2
WDI_JSON <wouterdijks@hotmail.com>
2022-03-06 18:12:32 +0100

    afronding app

---
+++ Todolist/CheckList/Views/NoItemsView.swift
@@ -0,0 +1,71 @@
+//
+//  NoItemsView.swift
+//  Todolist
+//
+//  Created by Wouter on 06/03/2022.
+//
+
+import SwiftUI
+
+struct NoItemsView: View {
+    
+    @State var animate: Bool = false
+    let secundaryAccentColor = Color("SecondaryAccentColor")
+    
+    var body: some View {
+        ScrollView{
+            VStack(spacing: 10){
+                Text("There are no items!")
+                    .font(.title)
+                    .fontWeight(.semibold)
+                Text("Productive people are more likely to be succesful and without stress in their live. Try adding some items to your todolist and get to work!💪.")
+                    .padding(.bottom, 20)
+                NavigationLink(destination: AddView(),
+                               label:{
+                    Text("Add something🎉")
+                        .foregroundColor(.white)
+                        .font(.headline)
+                        .frame(height: 55)
+                        .frame(maxWidth: .infinity)
+                        .background(animate ? secundaryAccentColor : Color.accentColor)
+                        .cornerRadius(10)
+                })
+                .padding(.horizontal, animate ? 30 : 50)
+                .shadow(
+                    color: animate ? secundaryAccentColor.opacity(0.7) : Color.accentColor.opacity(0.7),
+                    radius: animate ? 30 : 10,
+                    x: 0,
+                    y: animate ? 50 : 30)
+                .scaleEffect(animate ? 1.1 : 1.0)
+                .offset(y: animate ? -7 : 0)
+            }
+            .frame(maxWidth: 400)
+            .multilineTextAlignment(.center)
+            .padding(40)
+            .onAppear(perform: addAnimation)
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+        
+        func addAnimation() {
+            guard !animate else { return }
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
+                withAnimation(
+                    Animation
+                        .easeInOut(duration: 2.0)
+                        .repeatForever()
+                ) {
+                    animate.toggle()
+                }
+            }
+        }
+}
+
+struct NoItemsView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView{
+            NoItemsView()
+                .navigationTitle("Title")
+        }
+    }
+}
