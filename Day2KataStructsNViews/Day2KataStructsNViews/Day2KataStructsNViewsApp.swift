52bc1d055cbac3a679d1a8d6b4bd7da286ea804d
WDI_JSON <wouterdijks@hotmail.com>
2022-04-28 11:33:58 +0200

    Herhalende kata met stagemanagement!

---
+++ Day2KataStructsNViews/Day2KataStructsNViews/Day2KataStructsNViewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Day2KataStructsNViewsApp.swift
+//  Day2KataStructsNViews
+//
+//  Created by Wouter on 28/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct Day2KataStructsNViewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView(state: AppState())
+        }
+    }
+}
