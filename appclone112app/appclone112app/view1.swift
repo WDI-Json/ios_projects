fa0ff2df531ddea0d4d5cebf471127286e1160b6
WDI_JSON <wouterdijks@hotmail.com>
2022-05-13 21:46:30 +0200

    logo + geolocs

--- appclone112app/appclone112app/view1.swift
+++ appclone112app/appclone112app/view1.swift
@@ -8,35 +8,52 @@
 import SwiftUI
 
 struct view1: View {
-    var body: some View {
-        VStack{
-            Text("Uw locatie")
-                .padding(150)
-            RedPillButton()
-            LabelledDivider(label: "of")
-            VStack{
-                HStack{
-                    
-                    RedCircleButton()
-                        .padding()
-                    RedCircleButton()
-                        .padding()
-                    RedCircleButton()
-                        .padding()
-                }
-                HStack{Text("Politie")
-                        .padding(.horizontal)
-                        .multilineTextAlignment(.center)
-                    Text("Brandweer")
-                        .padding(.horizontal)
-                    Text("Ambulance")
-                }
-            }
-        }
-        
+    @StateObject var locationManager = LocationManager()
+    var userLatitude: String {
+        return "\(locationManager.lastLocation?.coordinate.latitude ?? 0)"
     }
     
+    var userLongitude: String {
+        return "\(locationManager.lastLocation?.coordinate.longitude ?? 0)"
+    }
+    
+    var body: some View {
+                HStack {
+                    Text("""
+                         Uw locatie
+                         \(userLatitude)N
+                         \(userLongitude)E
+                         nauwkeurigheid
+                         23 meter
+                         """)
+                    .bold()
+                    .multilineTextAlignment(.center)
+                }
+        .padding(150)
+        RedPillButton()
+        LabelledDivider(label: "of")
+        VStack{
+            HStack{
+                
+                RedCircleButton()
+                    .padding()
+                RedCircleButton()
+                    .padding()
+                RedCircleButton()
+                    .padding()
+            }
+            HStack{Text("Politie")
+                    .padding(.horizontal)
+                    .multilineTextAlignment(.center)
+                Text("Brandweer")
+                    .padding(.horizontal)
+                Text("Ambulance")
+            }
+        }
+    }
 }
+
+
 
 
 struct view1_Previews: PreviewProvider {
