20ee0b990c48abd05120f83b880b587183e3d9d8
WDI_JSON <wouterdijks@hotmail.com>
2022-05-14 16:12:27 +0200

    repair

---
+++ appclone112app/appclone112app/ChatFiles/ChatHelper.swift
@@ -0,0 +1,18 @@
+//
+//  ChatHelper.swift
+//  appclone112app
+//
+//  Created by Wouter on 14/05/2022.
+//
+
+import Combine
+
+class ChatHelper : ObservableObject {
+    var didChange = PassthroughSubject<Void, Never>()
+    @Published var realTimeMessages = DataSource.messages
+    
+    func sendMessage(_ chatMessage: Message) {
+        realTimeMessages.append(chatMessage)
+        didChange.send(())
+    }
+}
