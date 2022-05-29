94dfe3a5161797be985b15098dcfc09d268aa4c8
WDI_JSON <wouterdijks@hotmail.com>
2022-05-29 13:34:43 +0200

    aanvullen view

--- appclone112app/appclone112app/SettingsView.swift
+++ appclone112app/appclone112app/SettingsView.swift
@@ -10,14 +10,28 @@ import SwiftUI
 struct SettingsView: View {
     var body: some View {
         List{
-            Text("Persoonlijke gegevens")
+            NavigationLink("Persoonlijke gegevens", destination: PersonalSettings())
+            
             Text("Over 112NL")
             Text("Privacystatement")
             Text("Licenties")
             Text("Bekijk de 112NL app instructies")
             Section(footer: Text("112 appclone made by Wouter Dijks")) {
             }
+        }.listStyle(.plain)
+    }
+}
+            
+struct PersonalSettings: View {
+    var body: some View {
+        List{
+            Text("Naam")
+            Text("Telefoonnummer")
+            Text("Chat")
+            Text("Communicatie")
+            Text("Toestemmingen")
         }
+        .listStyle(.plain)
     }
 }
 
