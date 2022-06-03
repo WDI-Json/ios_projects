4ac1844d8387c22ff556b534a0c503429ed36d69
WDI_JSON <wouterdijks@hotmail.com>
2022-06-03 09:46:17 +0200

    upcoming tab and searcher

---
+++ NetflixCloneWD/NetflixCloneWD/ViewModels/TitleViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  TitleViewModel.swift
+//  NetflixCloneWD
+//
+//  Created by Wouter on 03/06/2022.
+//
+
+import Foundation
+
+struct TitleViewModel {
+    let titleName: String
+    let posterURL: String
+}
