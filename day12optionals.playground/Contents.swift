961671631a40fba77c4f340bfe4ec90b6260835a
WDI_JSON <wouterdijks@hotmail.com>
2022-07-14 21:48:16 +0200

    optionals

--- day12optionals.playground/Contents.swift
+++ day12optionals.playground/Contents.swift
@@ -1,3 +1,23 @@
 import UIKit
 
-var greeting = "Hello, playground"
+var optionalName: String? = nil
+
+if let unwrapped = optionalName {
+    print("\(unwrapped.count) letters")
+} else {
+    print("Missing name.")
+}
+
+var salary: Int? = nil
+
+func paySalary(_ salary: Int?) {
+    guard let unwrap = salary else {
+        print("Nothing here!")
+        return
+    }
+    print("Hello, \(unwrap)!")
+}
+
+//force unwrap
+let unwrapSalary = salary!
+
