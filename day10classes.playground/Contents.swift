74ae0828ef822275b84fdd0f6ff93026b0c3d3f0
WDI_JSON <wouterdijks@hotmail.com>
2022-07-14 20:52:45 +0200

    day 10 11 12

--- day10classes.playground/Contents.swift
+++ day10classes.playground/Contents.swift
@@ -1,7 +1,5 @@
 import UIKit
 
-var greeting = "Hello, playground"
-
 class Recipe {
     var name: String
     
@@ -24,3 +22,30 @@ class Taco: Recipe {
 
 let taco = Taco(name: "plain taco", containsLettuce: true, vegetarian: false)
 print(taco.name)
+
+//Deinit
+
+class SecretMessage {
+    var message: String
+    
+    init(message: String) {
+        self.message = message
+        print("initialized!")
+    }
+    
+    func printMessage() -> Void {
+        print(self.message)
+    }
+    
+    deinit {
+        print("the secret message '\(message)' is being deleted!")
+    }
+}
+let messages = ["I am secret!", "Well hello there", "Bananakin"]
+
+for item in messages {
+    let secret = SecretMessage(message: item)
+    secret.printMessage()
+}
+
+
