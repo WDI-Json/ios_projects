ad82b1565b44904c43a8b59e2a961235f22337cd
WDI_JSON <wouterdijks@hotmail.com>
2022-07-17 09:31:42 +0200

    tussen commit loading image

---
+++ project1/project1/DetailViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DetailViewController.swift
+//  project1
+//
+//  Created by Wouter on 17/07/2022.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    @IBOutlet var imageView: UIImageView!
+    var selectedImage: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        //unwrap optional selectedImage
+        if let imageToLoad = selectedImage {
+            imageView.image = UIImage(named: imageToLoad)
+        }
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
