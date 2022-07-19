8bceb479993da61b516bb0b81299bd5d87e51d32
WDI_JSON <wouterdijks@hotmail.com>
2022-07-19 10:32:13 +0200

    aanmaak + lay-out project 2

---
+++ project2/project2/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  project2
+//
+//  Created by Wouter on 19/07/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var button1: UIButton!
+    @IBOutlet var button2: UIButton!
+    @IBOutlet var button3: UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
