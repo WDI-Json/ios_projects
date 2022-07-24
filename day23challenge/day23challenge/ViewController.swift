6152cf803d1180aadb257061eb62f8ad1fa540e7
WDI_JSON <wouterdijks@hotmail.com>
2022-07-24 15:15:59 +0200

    werkend!

--- day23challenge/day23challenge/ViewController.swift
+++ day23challenge/day23challenge/ViewController.swift
@@ -34,6 +34,7 @@ class ViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Country", for: indexPath)
         cell.textLabel?.text = flags[indexPath.row]
+        cell.imageView?.image = UIImage(named: flags[indexPath.row])
         return cell
     }
 }
