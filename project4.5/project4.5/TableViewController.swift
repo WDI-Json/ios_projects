5afc977bc8f4db4c31b9dc9e6be47e5b6633c374
WDI_JSON <wouterdijks@hotmail.com>
2022-07-27 15:43:32 +0200

    challenge completed!!

--- project4.5/project4.5/TableViewController.swift
+++ project4.5/project4.5/TableViewController.swift
@@ -18,8 +18,8 @@ class TableViewController: UITableViewController, WKNavigationDelegate{
         navigationController?.navigationBar.prefersLargeTitles = true
     }
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        websitesAllowed.count
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return websitesAllowed.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
