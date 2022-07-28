6393c35efbf3a7e54075683a47aff6b598ca170e
WDI_JSON <wouterdijks@hotmail.com>
2022-07-28 14:50:24 +0200

    project 5 almost done!

--- Day27ClosureNotes.playground/Contents.swift
+++ Day27ClosureNotes.playground/Contents.swift
@@ -45,22 +45,55 @@
 //unowned
 
 //simple class
-class Baker {
-    func bakeStuff() {
-        print("I am baking things!")
+//class Baker {
+//    func bakeStuff() {
+//        print("I am baking things!")
+//    }
+//}
+//
+//func bake() -> () -> Void {
+//
+//    let baker = Baker()
+//
+//    let baking = { [unowned baker] in //capture list. It's 1 item now but could be more!
+//        baker.bakeStuff() //notice how the baker object is now considered an optional!
+//        return
+//    }
+//    return baking
+//}
+//
+//let bakeFunction = bake()
+//bakeFunction() //crash
+import UIKit
+
+class Person {
+    let name: String
+    init(name: String) {
+        self.name = name
+        print("\(name) is being initialized")
+    }
+    deinit {
+        print("\(name) is being deinitialized")
     }
 }
 
-func bake() -> () -> Void {
+let queue = DispatchQueue.main
+
+func main() {
+    let x = Person(name: "Harrie Snotter")
     
-    let baker = Baker()
-    
-    let baking = { [unowned baker] in //capture list. It's 1 item now but could be more!
-        baker.bakeStuff() //notice how the baker object is now considered an optional!
-        return
+    queue.asyncAfter(deadline: .now() + 3) { [unowned x] in
+        print("at 3 secs async name = \(x.name)")
     }
-    return baking
+    
+    queue.asyncAfter(deadline: .now() + 5) { [x] in
+        print("at 5 secs async name = \(x.name)")
+    }
+
+    queue.asyncAfter(deadline: .now() + 10) { [weak x] in
+        print("at 10 secs async name = \(x?.name)")
+    }
 }
 
-let bakeFunction = bake()
-bakeFunction() //crash
+
+main()
--- project5/project5/ViewController.swift
+++ project5/project5/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(promptForAnswer))
+        
         //load file
         if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
             //load contents
@@ -22,7 +24,7 @@ class ViewController: UITableViewController {
             }
         }
         if allWords.isEmpty {
-            allWords = ["silworm"]
+            allWords = ["silkworm"]
         }
         
         startGame()
@@ -34,8 +36,8 @@ class ViewController: UITableViewController {
         tableView.reloadData()
     }
     
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        usedWords.count
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return usedWords.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -44,7 +46,59 @@ class ViewController: UITableViewController {
         return cell
     }
     
+    @objc func promptForAnswer() {
+        let ac = UIAlertController(title: "Enter answer", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        
+        let submitAction = UIAlertAction(title: "Submit", style: .default) { [weak self, weak ac] _ in
+            guard let answer = ac?.textFields?[0].text else { return }
+            self?.submit(answer)
+        }
+        
+        ac.addAction(submitAction)
+        present(ac, animated: true)
+    }
     
+    func submit(_ answer: String) {
+        let lowerAnswer = answer.lowercased()
+        
+        if isPossible(word: lowerAnswer) {
+            if isOriginal(word: lowerAnswer) {
+                if isReal(word: lowerAnswer) {
+                    usedWords.insert(answer, at: 0)
+                    
+                    let indexPath = IndexPath(row: 0, section: 0)
+                    tableView.insertRows(at: [indexPath], with: .automatic)
+                }
+            }
+        }
+    }
     
+    func isPossible(word: String) -> Bool{
+        guard var tempword = title?.lowercased() else { return false }
+        
+        for letter in word {
+            if let position = tempword.firstIndex(of: letter) {
+                tempword.remove(at: position)
+            } else {
+                return false
+            }
+        }
+        return true
+    }
+    
+    func isOriginal(word: String) -> Bool {
+        return !usedWords.contains(word)
+    }
+    
+    func isReal(word: String) -> Bool {
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        return misspelledRange.location == NSNotFound
+    }
 }
+
+
+
 
