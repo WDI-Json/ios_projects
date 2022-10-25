386f7f70de3816fc6ddf0199135c757c4b64e213
WDijks <w.dijks@arcady.nl>
2022-10-25 15:19:13 +0200

    fix

--- ListViewSection/ListViewSection/DatePickerView.swift
+++ ListViewSection/ListViewSection/DatePickerView.swift
@@ -7,56 +7,44 @@
 
 import SwiftUI
 
-//
-//  DatePicker.swift
-//  Deskie
-//
-//  Created by Wouter on 18/10/2022.
-//
-
-import SwiftUI
-
 struct DatePickerView: View {
     @State private var selectedDate = Date()
 
     var body: some View {
         VStack(alignment: .leading) {
             Text("Datum")
-                .foregroundColor(.black)
-            HStack {
-                Text("\(selectedDate.formatted())")
-                Spacer()
+                .foregroundColor(.gray)
                 ZStack {
                     DatePicker("", selection: $selectedDate, in: ...Date(), displayedComponents: .date)
                         .datePickerStyle(.compact)
                         .labelsHidden()
                         .accentColor(.black)
+                        .compositingGroup()
+                        .scaleEffect(x: 10, y: 1.5)
+                        .clipped()
                     SwiftUIWrapper {
-                        Image(systemName: "calendar")
-                            .resizable()
-                            .frame(width: 18, height: 20)
+                        HStack {
+                            Text(selectedDate.formatted())
+                            Spacer()
+                            Image(systemName: "calendar")
+                                .resizable()
+                                .frame(width: 18, height: 20)
+                        }
+                        .padding(16)
+                        .background(Color.white)
                     }
+                    .padding(.vertical, 32)
+                    .fixedSize(horizontal: false, vertical: true)
                     .allowsHitTesting(false)
-                }
-                .frame(width: 18, height: 20)
             }
-            .padding(16)
-            .overlay {
+            .overlay (
                 RoundedRectangle(cornerRadius: 8)
                     .strokeBorder(
-                        style: StrokeStyle(
-                            lineWidth: 0.5
-                        )
+                        style: StrokeStyle(lineWidth: 0.5)
                     )
                     .foregroundColor(.black)
-            }
+            )
         }
-    }
-}
-
-struct DatePicker_Previews: PreviewProvider {
-    static var previews: some View {
-        DatePickerView()
     }
 }
 
@@ -68,8 +56,7 @@ struct SwiftUIWrapper<T: View>: UIViewControllerRepresentable {
     func updateUIViewController(_ uiViewController: UIHostingController<T>, context: Context) {}
 }
 
-
-struct DatePickerView_Previews: PreviewProvider {
+struct DatePicker_Previews: PreviewProvider {
     static var previews: some View {
         DatePickerView()
     }
