90513740a0986cb48f61ff60709e8f08beedd191
WDijks <w.dijks@arcady.nl>
2023-03-20 20:33:16 +0100

    removing deleted files from tracking

--- PointFreeArchitectureVid/PointFreeArchitectureVid/WolframApiKey.swift
+++
@@ -1,8 +0,0 @@
-//
-//  WolframApiKey.swift
-//  PointFreeArchitectureVid
-//
-//  Created by Wouter on 30/04/2022.
-//
-
-public let wolframAlphaApiKey = "VGYJAY-56K8W22J7X"
--- appCloneKata/appCloneKata/AppState.swift
+++
@@ -1,8 +0,0 @@
-//
-//  AppState.swift
-//  appCloneKata
-//
-//  Created by Wouter on 10/05/2022.
-//
-
-import Foundation
